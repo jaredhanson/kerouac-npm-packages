902509c1c2b38c40c2daf0e7e683064e788fb922
Jared Hanson <jaredhanson@gmail.com>
2017-11-27 11:54:58 -0800

    More tests.

--- test/handlers/show.test.js
+++ test/handlers/show.test.js
@@ -8,7 +8,7 @@ var handler = require('../../lib/handlers/show');
 describe('handlers/show', function() {
   var site = kerouac();
   
-  describe('package with metadata loaded from npm', function() {
+  describe('package with repository hosted at GitHub', function() {
     var packageJsonStub = sinon.stub().resolves({
       name: 'passport-openid',
       description: 'OpenID authentication strategy for Passport.',
@@ -82,9 +82,29 @@ describe('handlers/show', function() {
       expect(page.locals.keywords).to.deep.equal([ 'passport', 'openid', 'identity' ]);
       expect(page.locals.homepage).to.equal('https://github.com/jaredhanson/passport-openid');
       expect(page.locals.version).to.equal('0.4.0');
+      expect(page.locals.repository).to.deep.equal({
+        homepage: 'https://github.com/jaredhanson/passport-openid',
+        url: 'git://github.com/jaredhanson/passport-openid.git',
+        favoriteCount: 57,
+        subscriberCount: 7,
+        forkCount: 66,
+        createdAt: new Date(Date.parse('2011-11-03T05:26:46.000Z')),
+        modifiedAt: new Date(Date.parse('2017-09-16T23:34:19.000Z'))
+      });
       expect(page.locals.licence).to.equal(undefined);
       expect(page.locals.createdAt).to.deep.equal(new Date(Date.parse('2011-11-04T00:28:17.973Z')));
       expect(page.locals.modifiedAt).to.deep.equal(new Date(Date.parse('2017-08-30T14:29:54.769Z')));
+      
+      var readme =
+'<h1 id="passport-openid">Passport-OpenID</h1>\n' +
+'<p><a href="https://github.com/jaredhanson/passport">Passport</a> strategy for authenticating\n' +
+'with <a href="http://openid.net/">OpenID</a>.</p>\n' +
+'<p>This module lets you authenticate using OpenID in your Node.js applications.  By\n' +
+'plugging into Passport, OpenID authentication can be easily and unobtrusively\n' +
+'integrated into any application or framework that supports\n' +
+'<a href="http://www.senchalabs.org/connect/">Connect</a>-style middleware, including\n' +
+'<a href="http://expressjs.com/">Express</a>.</p>\n'
+      expect(page.locals.readme).to.equal(readme);
     });
     
     it('should render layout', function() {
