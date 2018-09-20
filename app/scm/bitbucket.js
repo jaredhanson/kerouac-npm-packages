1bb24edf0471b73f46415a9f6d728373159e635c
Jared Hanson <jaredhanson@gmail.com>
2018-09-20 16:13:09 -0700

    Remove unused SCM code, moved to @modulate/developer.

--- app/scm/bitbucket.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-
-// TODO:
-exports.get = function(info, cb) {
-  var url = 'https://api.bitbucket.org/2.0/repositories/' + info.user + '/' + info.project;
-  
-  request.get(url, { json: true }, function(err, res, json) {
-  });
-};
