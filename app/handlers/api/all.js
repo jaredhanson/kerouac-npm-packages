77cce104156e221b473ecdb6a33dbbd04ff6ba6f
Jared Hanson <jaredhanson@gmail.com>
2018-09-22 11:48:33 -0700

    Add data to all meta endpoint.

--- app/handlers/api/all.js
+++ app/handlers/api/all.js
@@ -4,14 +4,50 @@
  * [Deprecating the /-/all registry endpoint](http://blog.npmjs.org/post/157615772423/deprecating-the-all-registry-endpoint)
  *
  *  https://registry.npmjs.org/-/all
+ *
+ *  2012/08/24 - https://web.archive.org/web/20120824150709/https://registry.npmjs.org/-/all
+ *  2015/05/15 - https://web.archive.org/web/20150515053712/https://registry.npmjs.org/-/all
+ *
+ *  2018/02/18 - https://web.archive.org/web/20180218131402/https://registry.npmjs.org/-/all
  */
 
 
 exports = module.exports = function() {
   var uri = require('url');
   
+  
   function render(page, next) {
+    var packages = page.site.pages.filter(function(p) {
+      return (p.meta && p.meta.package == true);
+    });
+    
+    
     var json = {};
+    
+    packages.forEach(function(p) {
+      var obj = {};
+      obj.name = p.locals.name;
+      obj.description = p.locals.description;
+      obj.keywords = p.locals.keywords;
+      obj['dist-tags'] = { latest: p.locals.version };
+      obj.versions = {};
+      obj.versions[p.locals.version] = 'latest';
+      if (p.locals.publishedAt) {
+        obj.time = { modified: p.locals.publishedAt.toISOString() };
+      }
+      
+      if (p.locals.flags) {
+        obj._flags = p.locals.flags;
+      }
+      if (p.locals.count) {
+        obj._count = p.locals.count;
+      }
+      if (p.locals.downloads) {
+        obj._downloads = p.locals.downloads;
+      }
+      
+      json[p.locals.name] = obj;
+    });
     
     page.write(JSON.stringify(json, null, 2));
     page.end();
---
+++ test/handlers/api/all.test.js
@@ -0,0 +1,120 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var factory = require('../../../app/handlers/api/all');
+
+
+describe('handlers/api/all', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.be.undefined;
+    expect(factory['@singleton']).to.be.undefined;
+  });
+  
+  describe('handler', function() {
+    
+    describe('with one featured package', function() {
+      var page, err;
+    
+      before(function(done) {
+        chai.kerouac.handler(factory())
+          .page(function(page) {
+            page.site = {};
+            page.site.pages = [
+              { meta: { package: true },
+                locals: {
+                  title: 'passport-facebook',
+                  name: 'passport-facebook',
+                  description: 'Facebook authentication strategy for Passport.',
+                  keywords: [ 'passport', 'facebook', 'identity' ],
+                  version: '2.1.1',
+                  homepage: 'https://github.com/jaredhanson/passport-facebook#readme',
+                  repository: {
+                    type: 'git',
+                    url: 'git://github.com/jaredhanson/passport-facebook.git',
+                  },
+                  bugs: {
+                    url: 'http://github.com/jaredhanson/passport-facebook/issues'
+                  },
+                  license: {
+                    type: 'MIT',
+                    name: 'MIT License',
+                    url: 'http://www.opensource.org/licenses/MIT'
+                  },
+                  downloads: {
+                    'last-day': 7657,
+                    'last-week': 28650,
+                    'last-month': 183283
+                  },
+                  count: {
+                    favorites: 1062,
+                    subscribers: 49,
+                    forks: 417
+                  },
+                  flags: {
+                    featured: true
+                  },
+                  createdAt: new Date('2011-10-23T22:27:46.568Z'),
+                  modifiedAt: new Date('2018-08-03T00:35:46.879Z'),
+                  publishedAt: new Date('2016-05-17T19:13:37.644Z')
+                }
+              }
+            ];
+          
+            page.params = {};
+          })
+          .end(function(p) {
+            page = p;
+            done();
+          })
+          .dispatch();
+      });
+    
+      it('should write JSON', function() {
+        var expected = [
+          '{',
+          '  "passport-facebook": {',
+          '    "name": "passport-facebook",',
+          '    "description": "Facebook authentication strategy for Passport.",',
+          '    "keywords": [',
+          '      "passport",',
+          '      "facebook",',
+          '      "identity"',
+          '    ],',
+          '    "dist-tags": {',
+          '      "latest": "2.1.1"',
+          '    },',
+          '    "versions": {',
+          '      "2.1.1": "latest"',
+          '    },',
+          '    "time": {',
+          '      "modified": "2016-05-17T19:13:37.644Z"',
+          '    },',
+          '    "_flags": {',
+          '      "featured": true',
+          '    },',
+          '    "_count": {',
+          '      "favorites": 1062,',
+          '      "subscribers": 49,',
+          '      "forks": 417',
+          '    },',
+          '    "_downloads": {',
+          '      "last-day": 7657,',
+          '      "last-week": 28650,',
+          '      "last-month": 183283',
+          '    }',
+          '  }',
+          '}'
+        ].join("\n");
+    
+        expect(page.body).to.equal(expected);
+      });
+    }); // with one featured package
+    
+  });
+  
+});
