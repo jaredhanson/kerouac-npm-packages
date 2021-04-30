d6407db15cde50c3723ff55e8cc8d1d7a96e5c0e
Jared Hanson <jaredhanson@gmail.com>
2021-04-30 10:31:19 -0700

    Add documentation.

--- app/www/api/site.js
+++ app/www/api/site.js
@@ -1,5 +1,8 @@
 /**
- * npm Public Registry API site.
+ * npm Public Registry meta endpoints site.
+ *
+ * This component provides a site that consists of files conforming to the meta
+ * endpoints that are part of the npm [Public Registry API](https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md).
  *
  * @param {Function|Function[]} allHandler - All handler.
  * @returns {Function}
@@ -16,6 +19,7 @@ exports = module.exports = function(allHandler) {
 };
 
 exports['@implements'] = 'http://i.kerouacjs.org/Site';
+exports['@path'] = '/-';
 exports['@require'] = [
   './handlers/all'
 ];
