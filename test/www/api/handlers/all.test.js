5017ba3fcc9c1866469263f5702a4449b885b863
Jared Hanson <jaredhanson@gmail.com>
2021-12-20 12:11:24 -0800

    Clarify tests.

--- test/www/api/handlers/all.test.js
+++ test/www/api/handlers/all.test.js
@@ -21,160 +21,149 @@ describe('handlers/api/all', function() {
     };
     
     
-    describe('with one featured package', function() {
+    it('with one featured package', function(done) {
       var page, err;
     
-      before(function() {
-        sinon.stub(registry, 'list').yields(null, [
-          { name: 'passport-facebook' }
-        ]);
-        
-        sinon.stub(registry, 'read').withArgs('passport-facebook').yields(null, {
-          name: 'passport-facebook',
-          description: 'Facebook authentication strategy for Passport.',
-          keywords: [ 'passport', 'facebook', 'identity' ],
-          versions: {
-            '0.1.0': {
-              name: 'passport-facebook',
-              description: 'Facebook authentication strategy for Passport.',
-              keywords: 
-               [ 'passport',
-                 'facebook',
-                 'auth',
-                 'authn',
-                 'authentication',
-                 'identity' ],
-              homepage: undefined,
-              repositories: [ {
-                type: 'git',
-                url: 'git://github.com/jaredhanson/passport-facebook.git'
-              } ],
-              author: 
-               { name: 'Jared Hanson',
-                 email: 'jaredhanson@gmail.com',
-                 url: 'http://www.jaredhanson.net/' },
-              readme: undefined
-            },
-            '2.1.1': {
-              name: 'passport-facebook',
-              description: 'Facebook authentication strategy for Passport.',
-              keywords: 
-               [ 'passport',
-                 'facebook',
-                 'auth',
-                 'authn',
-                 'authentication',
-                 'identity' ],
-              homepage: 'https://github.com/jaredhanson/passport-facebook#readme',
-              repositories: [ {
-                type: 'git',
-                url: 'git://github.com/jaredhanson/passport-facebook.git'
-              } ],
-              author: 
-               { name: 'Jared Hanson',
-                 email: 'jaredhanson@gmail.com',
-                 url: 'http://www.jaredhanson.net/' },
-              license: { type: 'MIT' },
-              readme: undefined
-            }
+      sinon.stub(registry, 'list').yields(null, [
+        { name: 'passport-facebook' }
+      ]);
+      
+      sinon.stub(registry, 'read').withArgs('passport-facebook').yields(null, {
+        name: 'passport-facebook',
+        description: 'Facebook authentication strategy for Passport.',
+        keywords: [ 'passport', 'facebook', 'identity' ],
+        versions: {
+          '0.1.0': {
+            name: 'passport-facebook',
+            description: 'Facebook authentication strategy for Passport.',
+            keywords: 
+             [ 'passport',
+               'facebook',
+               'auth',
+               'authn',
+               'authentication',
+               'identity' ],
+            homepage: undefined,
+            repositories: [ {
+              type: 'git',
+              url: 'git://github.com/jaredhanson/passport-facebook.git'
+            } ],
+            author: 
+             { name: 'Jared Hanson',
+               email: 'jaredhanson@gmail.com',
+               url: 'http://www.jaredhanson.net/' },
+            readme: undefined
           },
-          'dist-tags': { latest: '2.1.1' },
-          homepage: 'https://github.com/jaredhanson/passport-facebook#readme',
-          repositories: [ {
-            type: 'git',
-            url: 'git://github.com/jaredhanson/passport-facebook.git'
-          } ],
-          bugs: {
-            url: 'http://github.com/jaredhanson/passport-facebook/issues'
-          },
-          author: {
-            name: 'Jared Hanson',
-            email: 'jaredhanson@gmail.com',
-            url: 'http://www.jaredhanson.net/'
-          },
-          license: {
-            type: 'MIT'
-          },
-          //readme: '# passport-facebook\n\n',
-          downloads: {
-            'last-day': 7657,
-            'last-week': 28650,
-            'last-month': 183283
-          },
-          ctime: new Date('2011-10-23T22:27:46.568Z'),
-          mtime: new Date('2018-08-03T00:35:46.879Z'),
-          ptime: new Date('2016-05-17T19:13:37.644Z')
-        });
-        
-        sinon.stub(project, 'info').withArgs('git://github.com/jaredhanson/passport-facebook.git').yields(null, {
-          name: 'passport-facebook',
-          description: 'Facebook authentication strategy for Passport and Node.js.',
-          homepage: 'https://github.com/jaredhanson/passport-facebook',
-          favoriteCount: 1062,
-          subscriberCount: 49,
-          forkCount: 417,
-          createdAt: new Date('2011-10-18T03:44:47.000Z'),
-          modifiedAt: new Date('2018-09-20T10:59:45.000Z')
-        });
+          '2.1.1': {
+            name: 'passport-facebook',
+            description: 'Facebook authentication strategy for Passport.',
+            keywords: 
+             [ 'passport',
+               'facebook',
+               'auth',
+               'authn',
+               'authentication',
+               'identity' ],
+            homepage: 'https://github.com/jaredhanson/passport-facebook#readme',
+            repositories: [ {
+              type: 'git',
+              url: 'git://github.com/jaredhanson/passport-facebook.git'
+            } ],
+            author: 
+             { name: 'Jared Hanson',
+               email: 'jaredhanson@gmail.com',
+               url: 'http://www.jaredhanson.net/' },
+            license: { type: 'MIT' },
+            readme: undefined
+          }
+        },
+        'dist-tags': { latest: '2.1.1' },
+        homepage: 'https://github.com/jaredhanson/passport-facebook#readme',
+        repositories: [ {
+          type: 'git',
+          url: 'git://github.com/jaredhanson/passport-facebook.git'
+        } ],
+        bugs: {
+          url: 'http://github.com/jaredhanson/passport-facebook/issues'
+        },
+        author: {
+          name: 'Jared Hanson',
+          email: 'jaredhanson@gmail.com',
+          url: 'http://www.jaredhanson.net/'
+        },
+        license: {
+          type: 'MIT'
+        },
+        //readme: '# passport-facebook\n\n',
+        downloads: {
+          'last-day': 7657,
+          'last-week': 28650,
+          'last-month': 183283
+        },
+        ctime: new Date('2011-10-23T22:27:46.568Z'),
+        mtime: new Date('2018-08-03T00:35:46.879Z'),
+        ptime: new Date('2016-05-17T19:13:37.644Z')
       });
-    
-      after(function() {
-        //project.info.restore();
-        //registry.list.restore();
-        //registry.read.restore();
+      
+      sinon.stub(project, 'info').withArgs('git://github.com/jaredhanson/passport-facebook.git').yields(null, {
+        name: 'passport-facebook',
+        description: 'Facebook authentication strategy for Passport and Node.js.',
+        homepage: 'https://github.com/jaredhanson/passport-facebook',
+        favoriteCount: 1062,
+        subscriberCount: 49,
+        forkCount: 417,
+        createdAt: new Date('2011-10-18T03:44:47.000Z'),
+        modifiedAt: new Date('2018-09-20T10:59:45.000Z')
       });
-    
-      before(function(done) {
-        chai.kerouac.handler(factory(registry, project))
-          .page(function(page) {
-            page.params = {};
-          })
-          .end(function(p) {
-            page = p;
-            done();
-          })
-          .dispatch();
-      });
-    
-      it('should write JSON', function() {
-        var expected = [
-          '{',
-          '  "passport-facebook": {',
-          '    "name": "passport-facebook",',
-          '    "description": "Facebook authentication strategy for Passport.",',
-          '    "keywords": [',
-          '      "passport",',
-          '      "facebook",',
-          '      "identity"',
-          '    ],',
-          '    "dist-tags": {',
-          '      "latest": "2.1.1"',
-          '    },',
-          '    "versions": {',
-          '      "2.1.1": "latest"',
-          '    },',
-          '    "time": {',
-          '      "modified": "2016-05-17T19:13:37.644Z"',
-          '    },',
-          //'    "_flags": {',
-          //'      "featured": true',
-          //'    },',
-          '    "_count": {',
-          '      "favorites": 1062,',
-          '      "subscribers": 49,',
-          '      "forks": 417',
-          '    },',
-          '    "_downloads": {',
-          '      "last-day": 7657,',
-          '      "last-week": 28650,',
-          '      "last-month": 183283',
-          '    }',
-          '  }',
-          '}'
-        ].join("\n");
-    
-        expect(page.body).to.equal(expected);
-      });
+  
+      chai.kerouac.handler(factory(registry, project))
+        .page(function(page) {
+          page.params = {};
+        })
+        .end(function(p) {
+          page = p;
+          
+          var expected = [
+            '{',
+            '  "passport-facebook": {',
+            '    "name": "passport-facebook",',
+            '    "description": "Facebook authentication strategy for Passport.",',
+            '    "keywords": [',
+            '      "passport",',
+            '      "facebook",',
+            '      "identity"',
+            '    ],',
+            '    "dist-tags": {',
+            '      "latest": "2.1.1"',
+            '    },',
+            '    "versions": {',
+            '      "2.1.1": "latest"',
+            '    },',
+            '    "time": {',
+            '      "modified": "2016-05-17T19:13:37.644Z"',
+            '    },',
+            //'    "_flags": {',
+            //'      "featured": true',
+            //'    },',
+            '    "_count": {',
+            '      "favorites": 1062,',
+            '      "subscribers": 49,',
+            '      "forks": 417',
+            '    },',
+            '    "_downloads": {',
+            '      "last-day": 7657,',
+            '      "last-week": 28650,',
+            '      "last-month": 183283',
+            '    }',
+            '  }',
+            '}'
+          ].join("\n");
+  
+          expect(page.body).to.equal(expected);
+          
+          done();
+        })
+        .dispatch();
     }); // with one featured package
     
   }); // handler
