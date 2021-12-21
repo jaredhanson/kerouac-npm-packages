a825b1b302d9e394b3b5e8cfc2a1d81cd3b04ad1
Jared Hanson <jaredhanson@gmail.com>
2021-12-21 11:38:11 -0800

    Add documentation.

--- app/www/api/site.js
+++ app/www/api/site.js
@@ -1,8 +1,15 @@
 /**
- * npm Public Registry meta endpoints site.
+ * npm Public Registry API.
  *
- * This component provides a site that consists of files conforming to the meta
- * endpoints that are part of the npm [Public Registry API](https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md).
+ * This component provides a site that implements the npm [Public Registry API](https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md).
+ * In particular, this implements the "escaped" portion of the API, which
+ * includes endpoints (within a "-" directory) that are not part of the the
+ * [CommonJS](http://wiki.commonjs.org/wiki/CommonJS) [Package Registry](http://wiki.commonjs.org/wiki/Packages/Registry)
+ * specification.
+ *
+ * This API has been superseded by the "v1" API.  Based on the naming of the
+ * later, it can be assumed that this API was implemented before npm had a
+ * versioning strategy and is effectively "v0".
  *
  * @param {Function|Function[]} allHandler - All handler.
  * @returns {Function}
--- app/www/api/v1/site.js
+++ app/www/api/v1/site.js
@@ -1,12 +1,26 @@
+/**
+ * npm Public Registry API v1.
+ *
+ * This component provides a site that implements the npm [Public Registry API](https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md).
+ * In particular, this implements the "escaped" portion of the API, which
+ * includes endpoints (within a "-" directory) that are not part of the the
+ * [CommonJS](http://wiki.commonjs.org/wiki/CommonJS) [Package Registry](http://wiki.commonjs.org/wiki/Packages/Registry)
+ * specification.
+ *
+ * @param {Function|Function[]} allHandler - All handler.
+ * @returns {Function}
+ */
 exports = module.exports = function(allHandler) {
   var kerouac = require('kerouac');
 
 
   var site = new kerouac.Router();
   
-  // This handler is mounted at `/-/v1/all`, mirroring the convention of npm's
-  // [deprecated](http://blog.npmjs.org/post/157615772423/deprecating-the-all-registry-endpoint)
-  // endpoint which resided at `/-/all`.
+  // This endpoint is not available npm's API.  The equivalent functionality is
+  // delivered via the `/search` endpoint.  However, because this package
+  // generates a static site and is not capable of dyanamic search, this
+  // endpoint is provided as an alternative.  It is mounted at the path `/all`,
+  // following the convention in the preceeding "v0" API.
   site.page('/all.json', allHandler);
   site.page('/all/:page.json', allHandler);
   
@@ -14,6 +28,7 @@ exports = module.exports = function(allHandler) {
 };
 
 exports['@implements'] = 'http://i.kerouacjs.org/Site';
+exports['@path'] = '/-/v1';
 exports['@require'] = [
   './handlers/all',
 ];
