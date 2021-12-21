869a098662bf91004751e4850beea7345ad7a51e
Jared Hanson <jaredhanson@gmail.com>
2021-12-21 09:41:03 -0800

    Clarify documentation.

--- app/www/api/v1/site.js
+++ app/www/api/v1/site.js
@@ -4,6 +4,9 @@ exports = module.exports = function(allHandler) {
 
   var site = new kerouac.Router();
   
+  // This handler is mounted at `/-/v1/all`, mirroring the convention of npm's
+  // [deprecated](http://blog.npmjs.org/post/157615772423/deprecating-the-all-registry-endpoint)
+  // endpoint which resided at `/-/all`.
   site.page('/all.json', allHandler);
   site.page('/all/:page.json', allHandler);
   
