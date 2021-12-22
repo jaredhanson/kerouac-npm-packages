67bb6af0e8d4157d81730f0c41b028a57b9d59c8
Jared Hanson <jaredhanson@gmail.com>
2021-12-22 10:38:38 -0800

    Add docs.

--- app/api/www/site.js
+++ app/api/www/site.js
@@ -1,3 +1,25 @@
+/**
+ * npm Public Registry API.
+ *
+ * This component provides a site that implements the npm [Public Registry API](https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md).
+ * In particular, this implements the "unescaped" portion of the API, which
+ * [claims to](https://docs.npmjs.com/cli/v7/using-npm/registry) implement the
+ * [CommonJS](http://wiki.commonjs.org/wiki/CommonJS) [Package Registry](http://wiki.commonjs.org/wiki/Packages/Registry)
+ * specification.
+ *
+ * Despite the claim to implement the CommonJS Package Registry specification,
+ * npm's implementation differs in substantial ways.  This is not surprising as
+ * the specification, unfortunately, is not particularly clear or consistent.
+ * This component acknowledges these ambiguities, and attempts to be as
+ * compatible as possible with clients expecting either variation.  This is a
+ * pragmatic decision, acknowledging the fact that npm is the most widely used
+ * registry, and clients are likely to be developed against and compatible with
+ * its implementation, even where it deviates from the specification.
+ *
+ * @param {Function|Function[]} dbHandler - DB info handler.
+ * @param {Function|Function[]} packageHandler - Package handler.
+ * @returns {Function}
+ */
 exports = module.exports = function(dbHandler, packageHandler) {
   var kerouac = require('kerouac')
 
